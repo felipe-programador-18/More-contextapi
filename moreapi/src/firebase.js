50889671660a7cf27c9634c842bfc0ad87ef0c06
Felipe <machadofelipe2016@outlook.com>
2022-05-01 20:06:23 -0300

    c2-adding connectins about firebase

---
+++ moreapi/src/firebase.js
@@ -0,0 +1,4 @@
+//create structure to adding firebase database and firebase auth!!!!
+import firebase from 'firebase/compat/app'
+import firebase from 'firebase/compat/database'
+import firebase from 'firebase/compat/auth'

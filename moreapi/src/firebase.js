eeff9ab4b853ab9d47eaef6b782685ea3555d36c
Felipe <machadofelipe2016@outlook.com>
2022-05-02 15:04:02 -0300

    c5-get started with database to caugght all date

--- moreapi/src/firebase.js
+++ moreapi/src/firebase.js
@@ -2,3 +2,18 @@
 import firebase from 'firebase/compat/app'
 import 'firebase/compat/database'
 import 'firebase/compat/auth'
+
+
+
+// later create structure of database firebase i have caught key autenticatoin!!
+const firebaseConfig = {
+    apiKey: "AIzaSyDpTzaRhgyq0VtQypvtliwnp5zwJQTYF7Q",
+    authDomain: "autenticacao-react-cc4e7.firebaseapp.com",
+    projectId: "autenticacao-react-cc4e7",
+    storageBucket: "autenticacao-react-cc4e7.appspot.com",
+    messagingSenderId: "832823698981",
+    appId: "1:832823698981:web:a87453f3495e40242881cc"
+  };
+firebase.initializeApp(firebaseConfig)
+
+export default firebase

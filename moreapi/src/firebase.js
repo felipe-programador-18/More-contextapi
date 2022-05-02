650d3e16788d973e7e201c507834cf7537868300
Felipe <machadofelipe2016@outlook.com>
2022-05-02 14:47:54 -0300

    c4-adding firebase database to coding

--- moreapi/src/firebase.js
+++ moreapi/src/firebase.js
@@ -1,4 +1,4 @@
 //create structure to adding firebase database and firebase auth!!!!
 import firebase from 'firebase/compat/app'
-import firebase from 'firebase/compat/database'
-import firebase from 'firebase/compat/auth'
+import 'firebase/compat/database'
+import 'firebase/compat/auth'

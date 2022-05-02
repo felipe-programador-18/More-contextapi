9b91f9a59655be2617fff8dec74ca5d42e11c955
Felipe <machadofelipe2016@outlook.com>
2022-05-02 18:23:09 -0300

    c25-finnaly file Crreate user

--- moreapi/src/CreateUser.js
+++ moreapi/src/CreateUser.js
@@ -15,5 +15,16 @@ const CreateUser = () => {
 
     if(auth.users !== null){
         return null
-    }
+    } 
+
+    return ( <>
+        <h2>Por favor crie uma nova conta!</h2>
+        {  auth.CreateUser.CreateUser.error !== '' &&
+        <p> {auth.CreateUser.CreateUser.error}</p>
+        } 
+         <input type='text' placeholder="Digite seu email:"   onChange={onChange('email')} value={form.email}  />
+         <input type='password' placeholder="Digite sua senha:" onChange={onChange('password')}  value={form.password}  />
+
+         <button onClick={() =>{auth.CreateUser.CreateUser(form.email, form.password)}} >Criar conta!</button>
+    </>)
 }

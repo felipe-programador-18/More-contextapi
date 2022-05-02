2f8ad480814959355c6c65f2d1930c2d8bb607c2
Felipe <machadofelipe2016@outlook.com>
2022-05-02 18:37:45 -0300

    c28-adding more code

--- moreapi/src/App.js
+++ moreapi/src/App.js
@@ -2,13 +2,15 @@ import React from 'react'
 import './App.css'
 import { AuthProvider } from './auth'
 import Comments from './Comments'
+import CreateUser from './CreateUser';
 
 function App() {
   return (
     <AuthProvider>
     <div className="App">
+     <CreateUser/>
      <Comments/>  
-    
+  
     </div>
     </AuthProvider>
   );

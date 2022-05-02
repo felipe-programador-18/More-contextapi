2fb4acf49c5e9dd9de5178ce6ffb1264bbb806cc
Felipe <machadofelipe2016@outlook.com>
2022-05-02 15:08:21 -0300

    c7-create Usedatabase

--- moreapi/src/database.js
+++ moreapi/src/database.js
@@ -4,12 +4,21 @@ import firebase from './firebase'
 
 //here i have create one inject dependence to caught date with firebase
 export const UsedadabasePush =  endpoints => {
-  const [status, setstatus] = useState('')
-   
+  const [status, setstatus] = useState('') 
+  //inject dependence to verify data and error
   const save = data => {
     const ref = firebase.database().ref(endpoints)
-
+    ref.push(data, err => {
+        if(err){
+         setstatus('Success aplication!!')
+        }else{
+            setstatus('error in aplication!!')
+        }
+    })
   }
-
   return [status, save]
+}
+
+export const Usedabase = endpoints => {
+    const [data, setdate] = useState({})
 }
